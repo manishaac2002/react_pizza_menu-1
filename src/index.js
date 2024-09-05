a7a0a13d6caa1e14e6012616e0045e9f1309295d
manishaac2002 <manishaac2002@gmail.com>
2024-09-05 16:53:11 +0530

    feat:card

--- src/index.js
+++ src/index.js
@@ -1,121 +1,50 @@
-import React from "react";
-import ReactDOM from "react-dom/client"
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 import "./index.css";
-
-const pizzaData = [
-  {
-    name: "Focaccia",
-    ingredients: "Bread with italian olive oil and rosemary",
-    price: 6,
-    photoName: "pizzas/focaccia.jpg",
-    soldOut: false,
-  },
-  {
-    name: "Pizza Margherita",
-    ingredients: "Tomato and mozarella",
-    price: 10,
-    photoName: "pizzas/margherita.jpg",
-    soldOut: false,
-  },
-  {
-    name: "Pizza Spinaci",
-    ingredients: "Tomato, mozarella, spinach, and ricotta cheese",
-    price: 12,
-    photoName: "pizzas/spinaci.jpg",
-    soldOut: false,
-  },
-  {
-    name: "Pizza Funghi",
-    ingredients: "Tomato, mozarella, mushrooms, and onion",
-    price: 12,
-    photoName: "pizzas/funghi.jpg",
-    soldOut: false,
-  },
-  {
-    name: "Pizza Salamino",
-    ingredients: "Tomato, mozarella, and pepperoni",
-    price: 15,
-    photoName: "pizzas/salamino.jpg",
-    soldOut: true,
-  },
-  {
-    name: "Pizza Prosciutto",
-    ingredients: "Tomato, mozarella, ham, aragula, and burrata cheese",
-    price: 18,
-    photoName: "pizzas/prosciutto.jpg",
-    soldOut: false,
-  },
-];
 
 function App() {
   return (
-    <div className="container">
-      <Header />
-      <Menu />
-      <Footer />
-    </div>
-  )
-}
-
-function Header() {
-  return <header className="header">
-    <h1> Fast React Pizza Co.</h1>
-  </header>
-}
-function Menu() {
-  return (<main className="menu">
-    <h2>Our Menu</h2>
-    <Pizza
-      name="Pizza Spinaci"
-      ingredients="Tomato, mozarella, spinach, and ricotta cheese"
-      price={10}
-      photoName="pizzas/spinaci.jpg"
-    />
-
-    <Pizza
-      name="Pizza Funghi"
-      ingredients="Tomato, mozarella, mushrooms, and onion"
-      price={12}
-      photoName="pizzas/funghi.jpg"
-    />
-  </main>
-  )
-}
-
-function Pizza(props) {
-  return (
-    <div className="pizza">
-      <img src={props.photoName} alt={props.name} />
-
-      <div>
-      <h3>{props.name}</h3>
-      <p>{props.ingredients}</p>
+    <div className="card">
+      <Avatar />
+      <div className="data">
+        <Intro />
+        {/* Should contain one Skill component
+        for each web dev skill that you have,
+        customized with props */}
+        <SkillList />
       </div>
+    </div>
+  );
+}
 
-      <span>{props.price + 3}</span>
+function Avatar() {
+  return (
+    <div className="avatar">
+      <img src="./human.jpeg" alt="Avatar" />
+    </div>)
+}
+
+function Intro() {
+  return (
+    <div>
+      <h1>Hi, I'm Manisha</h1>
+      <p>I'm a web developer. I'm currently learning React in that course specifically for this project. 
+        I'm a web developer. I'm currently learning React in that course specifically for this project. I'm a web developer. 
+        I'm currently learning React in that course specifically for this project
+      </p>
     </div>
   )
 }
-function Footer() {
-  const hour = new Date().getHours()
-  const openHour = 12
-  const closeHour = 22
 
-  if (hour >= openHour && hour <= closeHour)
-    alert("We are currently open.")
-  else
-    alert("We are currently closed.")
+function SkillList() {
 
-  return <footer className="footer">{new Date().toLocaleDateString()}. We are currently open .</footer>
 }
 
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
 
-
-const root = ReactDOM.createRoot(document.getElementById("root"))
-root.render
-  (<React.StrictMode>
+root.render(
+  <StrictMode>
     <App />
-  </React.StrictMode>)
-
-
-
+  </StrictMode>
+);
